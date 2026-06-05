f2149db727db1d7af4d2d30b9f5522578daebf94
Sneha-2709 <jaiswalsneha878@gmail.com>
2026-06-05 19:54:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ async function checkWeather(city) {
     } else {
         const data = await response.json();
         document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "C";
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
         weatherIcon.src = getIconSrc(data.weather[0].main);
